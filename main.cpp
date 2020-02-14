5fc1e16c06a098f2cf5af465ff430b9169203ab4
Kerim Enes YILMAZ <enes.yilmaz@sparsetechnology.com>
2020-02-14 13:38:58 +0300

    initial commit: add pmedia library and grpc compiling pri.

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include "mainwindow.h"
+#include <QApplication>
+
+#include <QFile>
+static void setStyleSheets(const QString &file)
+{
+	QString styleSheet;
+	QFile f(file);
+	if (f.open(QIODevice::ReadOnly))
+		styleSheet = f.readAll();
+	f.close();
+	if (!styleSheet.isEmpty())
+		qApp->setStyleSheet(styleSheet);
+}
+
+int main(int argc, char *argv[])
+{
+	QApplication a(argc, argv);
+	setStyleSheets(":dark.qss");
+	MainWindow w;
+	w.show();
+
+	return a.exec();
+}
---
+++ pmedia.pri
@@ -0,0 +1,5 @@
+INCLUDEPATH += $$PWD/../../pmedia/
+LIBS += /home/kerim/myfs/source-codes/sparsetech/build/pmedia/libpmedia.a
+PRE_TARGETDEPS += /home/kerim/myfs/source-codes/sparsetech/build/pmedia/libpmedia.a
+
+LIBS += -lpthread -ldl -lliveMedia -lgroupsock -lBasicUsageEnvironment -lUsageEnvironment -lfmt -lunwind -lleveldb
